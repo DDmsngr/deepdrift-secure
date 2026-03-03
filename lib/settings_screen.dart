a5b30a0dd296bef3bdc6291eb3383e3acb36c728
DDmsngr <khti@bk.ru>
2026-03-03 15:31:07 +0300

    Update settings_screen.dart

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -3,7 +3,6 @@ import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 import 'storage_service.dart';
