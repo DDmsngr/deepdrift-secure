6f3fbe34d9871f15c2be0ddfa1169bd3b1b7cf52
DDmsngr <khti@bk.ru>
2026-03-18 11:11:58 +0300

    Update settings_screen.dart

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -457,7 +457,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           _infoTile(
             icon:  Icons.info_outline,
             title: 'Версия',
-            value: 'DDChat 3.4.0',
+            value: 'DDChat 3.5.0',
           ),
 
           ListTile(
